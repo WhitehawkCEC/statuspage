7c1769376bd6aa5a2d906fa74c6b3e1a2f9bac1b
Aromadz <amit.niroula.5@gmail.com>
2022-02-09 16:50:24 -0500

    chore: changing origin name and user to whitehawk system

--- health-check.sh
+++ health-check.sh
@@ -3,8 +3,7 @@
 # later upstream merges messy for anyone who forked us.
 commit=true
 origin=$(git remote get-url origin)
-if [[ $origin == *statsig-io/statuspage* ]]
-then
+if [[ $origin == *statsig-io/statuspage* ]]; then
   commit=false
 fi
 
@@ -13,27 +12,24 @@ URLSARRAY=()
 
 urlsConfig="./urls.cfg"
 echo "Reading $urlsConfig"
-while read -r line
-do
+while read -r line; do
   echo "  $line"
-  IFS='=' read -ra TOKENS <<< "$line"
+  IFS='=' read -ra TOKENS <<<"$line"
   KEYSARRAY+=(${TOKENS[0]})
   URLSARRAY+=(${TOKENS[1]})
-done < "$urlsConfig"
+done <"$urlsConfig"
 
 echo "***********************"
 echo "Starting health checks with ${#KEYSARRAY[@]} configs:"
 
 mkdir -p logs
 
-for (( index=0; index < ${#KEYSARRAY[@]}; index++))
-do
+for ((index = 0; index < ${#KEYSARRAY[@]}; index++)); do
   key="${KEYSARRAY[index]}"
   url="${URLSARRAY[index]}"
   echo "  $key=$url"
 
-  for i in 1 2 3 4; 
-  do
+  for i in 1 2 3 4; do
     response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
     if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 302 ] || [ "$response" -eq 307 ]; then
       result="success"
@@ -46,21 +42,19 @@ do
     sleep 5
   done
   dateTime=$(date +'%Y-%m-%d %H:%M')
-  if [[ $commit == true ]]
-  then
-    echo $dateTime, $result >> "logs/${key}_report.log"
+  if [[ $commit == true ]]; then
+    echo $dateTime, $result >>"logs/${key}_report.log"
     # By default we keep 2000 last log entries.  Feel free to modify this to meet your needs.
-    echo "$(tail -2000 logs/${key}_report.log)" > "logs/${key}_report.log"
+    echo "$(tail -2000 logs/${key}_report.log)" >"logs/${key}_report.log"
   else
     echo "    $dateTime, $result"
   fi
 done
 
-if [[ $commit == true ]]
-then
+if [[ $commit == true ]]; then
   # Let's make Vijaye the most productive person on GitHub.
-  git config --global user.name 'Vijaye Raji'
-  git config --global user.email 'vijaye@statsig.com'
+  git config --global user.name 'Whitehawk DevOps'
+  git config --global user.email 'whdevops@whitehawk.com'
   git add -A --force logs/
   git commit -am '[Automated] Update Health Check Logs'
   git push
